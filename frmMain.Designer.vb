2decba26f298214bd3d62c076499aa3fd90f53b3
Neil Christian Riego <neilchristianriego3@gmail.com>
2023-04-25 15:27:12 +0800

    Add project files.

---
+++ frmMain.Designer.vb
@@ -0,0 +1,179 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnStart = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnStart
+        '
+        Me.btnStart.BackColor = System.Drawing.Color.DarkSeaGreen
+        Me.btnStart.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnStart.Location = New System.Drawing.Point(13, 12)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(50, 30)
+        Me.btnStart.TabIndex = 0
+        Me.btnStart.Text = "Start"
+        Me.btnStart.UseVisualStyleBackColor = False
+        '
+        'btnReset
+        '
+        Me.btnReset.BackColor = System.Drawing.Color.DarkSalmon
+        Me.btnReset.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnReset.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnReset.Location = New System.Drawing.Point(13, 12)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(50, 30)
+        Me.btnReset.TabIndex = 1
+        Me.btnReset.Text = "Reset"
+        Me.btnReset.UseVisualStyleBackColor = False
+        Me.btnReset.Visible = False
+        '
+        'Label1
+        '
+        Me.Label1.Image = Global.vb_activity1.My.Resources.Resources._1
+        Me.Label1.Location = New System.Drawing.Point(10, 64)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(100, 140)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.Image = Global.vb_activity1.My.Resources.Resources._1
+        Me.Label2.Location = New System.Drawing.Point(116, 64)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(100, 140)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Label2"
+        '
+        'Label3
+        '
+        Me.Label3.Image = Global.vb_activity1.My.Resources.Resources._1
+        Me.Label3.Location = New System.Drawing.Point(222, 64)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(100, 140)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Label3"
+        '
+        'Label4
+        '
+        Me.Label4.Image = Global.vb_activity1.My.Resources.Resources._1
+        Me.Label4.Location = New System.Drawing.Point(328, 64)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(100, 140)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Label4"
+        '
+        'Label5
+        '
+        Me.Label5.Image = Global.vb_activity1.My.Resources.Resources._1
+        Me.Label5.Location = New System.Drawing.Point(434, 64)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(100, 140)
+        Me.Label5.TabIndex = 6
+        Me.Label5.Text = "Label5"
+        '
+        'Label6
+        '
+        Me.Label6.Image = Global.vb_activity1.My.Resources.Resources._1
+        Me.Label6.Location = New System.Drawing.Point(540, 64)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(100, 140)
+        Me.Label6.TabIndex = 7
+        Me.Label6.Text = "Label6"
+        '
+        'Label7
+        '
+        Me.Label7.Image = Global.vb_activity1.My.Resources.Resources._1
+        Me.Label7.Location = New System.Drawing.Point(646, 64)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(100, 140)
+        Me.Label7.TabIndex = 8
+        Me.Label7.Text = "Label7"
+        '
+        'Label8
+        '
+        Me.Label8.Image = Global.vb_activity1.My.Resources.Resources._1
+        Me.Label8.Location = New System.Drawing.Point(752, 64)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(100, 140)
+        Me.Label8.TabIndex = 9
+        Me.Label8.Text = "Label8"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(157, 16)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(59, 20)
+        Me.Label9.TabIndex = 10
+        Me.Label9.Text = "Score: "
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(858, 530)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnStart)
+        Me.Name = "frmMain"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnStart As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+End Class
---
+++ frmMain.vb
@@ -0,0 +1,65 @@
+﻿Imports System.Net
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class frmMain
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim numbers As New List(Of Integer)
+        Dim random As New Random()
+
+        For i As Integer = 0 To 11
+            Dim rand_number = random.Next(1, 53)
+            numbers.Add(rand_number)
+            numbers.Add(rand_number)
+        Next
+
+        Dim final_numbers As New List(Of Integer)
+
+        Do While final_numbers.Count <> 24
+            Dim rand_number = random.Next(numbers.Count)
+            final_numbers.Add(numbers(rand_number))
+            numbers.Remove(numbers(rand_number))
+        Loop
+
+        Dim stringOfNumbers As String = String.Join(",", final_numbers)
+
+        MsgBox(stringOfNumbers)
+
+    End Sub
+
+    Private Sub Label_Click(sender As Object, e As EventArgs)
+        Dim label As Label = CType(sender, Label)
+        MessageBox.Show(label.Name & " clicked")
+    End Sub
+
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+        btnStart.Visible = False
+        btnReset.Visible = True
+
+        'For i As Integer = 1 To 5
+        '    'label.Text = "Label " & i + 5
+        '    Dim label As New Label With {
+        '        .Name = "Label" & i + 5,
+        '        .Image = My.Resources.back3,
+        '        .Location = New Point(10, (i - 1) * y_size + 30),
+        '        .Size = New Point(100, y_size)
+        '    }
+
+        '    AddHandler label.Click, AddressOf Label_Click
+        '    Me.Controls.Add(label)
+        'Next
+        'For i As Integer = 1 To 5
+        '    Dim label As New Label()
+        '    label.Name = "Label" & i
+        '    label.Text = "Label " & i
+        '    label.Location = New Point(10, (i - 1) * 30 + 10)
+        '    AddHandler label.Click, AddressOf Label_Click
+        '    Me.Controls.Add(label)
+        'Next
+    End Sub
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        btnStart.Visible = True
+        btnReset.Visible = False
+    End Sub
+End Class
